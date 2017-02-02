5c157f2f78e29c5007d2e9d9b4c92c36f7a33c0c
Doug von Kohorn <dougvk@gmail.com>
2017-02-02 14:21:56 -0500

    Update with clearer example.

--- app/main.js
+++ app/main.js
@@ -2,13 +2,16 @@ import { default as Web3 } from 'web3'
 import { default as contract } from 'truffle-contract'
 import MetaCoinContract from '../build/contracts/MetaCoin.json'
 
+const TESTRPC_HOST = 'localhost'
+const TESTRPC_PORT = '7000'
+
 function component () {
   var element = document.createElement('div')
-  let provider = new Web3.providers.HttpProvider('http://localhost:7000')
+  let provider = new Web3.providers.HttpProvider(`http:\/\/${TESTRPC_HOST}:${TESTRPC_PORT}`)
   let meta = contract(MetaCoinContract)
   meta.setProvider(provider)
   meta.deployed()
-    .then((instance) => console.log(instance))
+    .then((instance) => element.innerHTML = `Metacoin address: ${instance.address}`)
 
   return element
 }
